79fe5294bae61089e34add448b60283ef17f2e80
Nqmez <blazegamertv.yotube@gmail.com>
2026-08-31 16:39:45 +0700

    classroom work done

---
+++ Lab03/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Student ID :1690702079
+ * Name       :สุภนัย หัทยาภิชาติ
+ * Section    :129C
+ * No.        :N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
+ 
+ namespace Lab03;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       const int MaxLevel = 10;
+
+        var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+        var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+        int level = 7;
+        int maxHp = 240;
+        int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+        float attackPower = 42.5f;
+        double critMultiplier = 1.75;
+        bool isBoss = true;
+
+        Console.WriteLine("===== KIRIN SAVE CONVERTER ====== ");
+        Console.WriteLine($"/nNAME: {bossName}" +
+            $"\nRANK: {rank}" +
+            $"\nLEVEL: {level} / {MaxLevel}" +
+            $"\nHP: {currentHp} / {maxHp}" +
+            $"\nATTACK POWER: {attackPower}" +
+            $"\nCRIT MULTIPLIER: {critMultiplier}" +
+            $"\nIS BOSS: {isBoss}");
+
+        //Implicit converion HP (int) -->> double
+        Console.WriteLine("\n----- Implicit Conversion: HP as double------");
+        double currentHpDouble = currentHp;
+        Console.WriteLine($"HP (double): {currentHpDouble}");
+
+        //calculate percent as double
+        Console.WriteLine("\n----Exact HP Perent (no interger truncation)-----");
+        double hpPercentExact = currentHpDouble = 100 / maxHp;
+        Console.WriteLine($"HP Percent (exact): {hpPercentExact}%");
+        
+    }
+}
